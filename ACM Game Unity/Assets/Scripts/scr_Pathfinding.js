af5f9409530d8a85938133a1a6c093b38918315a
wdixon <wdixon23192@gmail.com>
2013-11-20 19:42:34 -0500

    THIS IS WILLS COMMIT ASGHERIGNEOI i

---
+++ ACM Game Unity/Assets/Scripts/scr_Pathfinding.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+// A script that subdivides the play area
+private var gameWidth : double;
+private var gameHeight : double;
+function Start () 
+{
+	//get width and height of map
+	gameWidth = GameObject.Find("obj_Player").renderer.bounds.max.x;
+	gameHeight = GameObject.Find("obj_Player").renderer.bounds.max.y;
+	//create grid overlay
+	for(var i:int = 0; i<gameWidth; i++)
+	{
+		for(var j:int = 0; j<gameHeight; j++)
+		{
+			
+		}
+	}
+	//calculate path
+	//return the path
+
+}
+
+function Update () 
+{
+
+}
