672004486a35f22f2001b73a420f35f0bd1381b2
lavalle00 <spencerlavalle95@gmail.com>
2013-11-22 16:52:29 -0500

    added enemy count

---
+++ ACM Game Unity/Assets/guiScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+private var enemyCount : int;
+var enemyList : GameObject[];
+
+
+
+function Start () {
+
+enemyList = GameObject.FindGameObjectsWithTag('Enemy') as GameObject[];
+enemyCount = enemyList.Length;
+Debug.Log(enemyCount);
+}
+
+function Update () {
+
+}
+function enemyCountUpdate(){
+
+}
