e42d18670f76913721c4cfb6c2358431f6da2634
Daniel Piros <dpiros@comcast.net>
2013-11-24 13:36:29 -0500

    Set up collision box loader

---
+++ ACM Game Unity/Assets/Scripts/loadCollisions.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class loadCollisions : MonoBehaviour
+{
+	/*
+	 * TODO: Transform is off, because it needs to account for the transform of the tilemap object.
+	 * Should put all CollisionBox objects under a single object (preferably the one that has this script attached).
+	 */
+	public Transform collisionBoxPrefab;
+	public TextAsset collisionFile;
+
+	// Use this for initialization
+	void Start () {
+		Load("collisions.txt");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	
+	private bool Load(string fileName)
+	{
+		// Handle any problems that might arise when reading the text
+		try
+		{
+			string text = collisionFile.text;
+			string[] lines = text.Split('\n');
+
+			for (int i = 0; i < lines.Length; ++i)
+			{
+				string line = lines[i];
+				string[] coordinates = line.Split (' ');
+				Vector3 transform = new Vector3(System.Convert.ToSingle(coordinates[0]), System.Convert.ToSingle(coordinates[1]), 0);
+				Instantiate(collisionBoxPrefab, transform, Quaternion.identity);
+			}
+			return true;
+		}
+		
+		// If anything broke in the try block, we throw an exception with information
+		// on what didn't work
+		catch (System.Exception e)
+		{
+			System.Console.WriteLine("{0}\n", e.Message);
+			return false;
+		}
+	}
+
+}
